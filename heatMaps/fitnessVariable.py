c3375aab810c4708fc2e23678d628f1696a2c347
jbongard <jbongard@.uvm.edu>
2020-03-24 13:52:14 -0400

    Building out class hierarchy for heat maps.

---
+++ heatMaps/fitnessVariable.py
@@ -0,0 +1,13 @@
+class FITNESS_VARIABLE:
+
+    def __init__(self,name):
+
+        self.name = name
+
+    def Draw(self,axis):
+
+        return
+
+    def Get_Name(self):
+
+        return self.name
